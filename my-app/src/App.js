09e2410783f1e3b880b61b0f3acf6c373eb00e7b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-07 21:26:34 +0700

    login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-import Navbar from './components/Navbar';
 
 // Contents
-import Home from './contents/Home';
 import Sign from './contents/Sign';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -11,12 +9,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/sign">
-          <Sign />
-        </Route>
-        <Navbar />
         <Route exact path="/">
-          <Home />
+          <Sign />
         </Route>
       </div>
     </Router>
--- my-app/src/contents/Sign.js
+++ my-app/src/contents/Sign.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
 import signpic from '../img/Ellipse.png'
 
-import Backitems from '../components/Backitems'
-
 class Sign extends Component {
     render() {
         return (
             <div className="condiv sign">
                 <img src={signpic} alt="Ellipse.png" className="signpic" />
                 <h1 className="signtopic">C I N E M A H</h1>
-                <back>
-                    <ul>
-                        <Backitems item="Return to Home" tolink="/"></Backitems>
-                    </ul>
-                </back>
                 <h2 className="welcome">WELCOME !!</h2>
                 <p className="comment1">please sign in before using</p>
                 <p className="comment2">if you don’t have an account sign up free!</p>
