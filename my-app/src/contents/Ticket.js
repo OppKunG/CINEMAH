b8395eb591051a4502814d6c208778e42a370474
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-13 19:15:12 +0700

    ticket

--- my-app/src/contents/Ticket.js
+++ my-app/src/contents/Ticket.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import ticketpic from '../img/ticket.png'
 import barcodepic from '../img/barcode.png'
+import accountpic from '../img/account.png'
 
 class Ticket extends Component {
     render() {
         return (
             <div className="condiv ticket">
                 <h1 className="tickettopic">C I N E M A H</h1>
-                <obj className="account"></obj>
+                <img src={accountpic} alt="barcode.png" className="accountpic" />
                 <h1 className="accountname">K</h1>
                 <h1 className="name">testttt_1</h1>
-                <h1 logout>Sign out</h1>
+                <h1 classname="logout">Sign out</h1>
                 <h2 className="hometopic">HOME</h2>
                 <h2 className="movietopic">MOVIE</h2>
                 <h2 className="historytopic">HISTORY</h2>
